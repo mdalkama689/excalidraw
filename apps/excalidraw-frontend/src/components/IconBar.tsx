79f68ac026b2fd1cbda930ed4e1f838b6d49e70a
mdalkama <mdalkama689@gmail.com>
2025-02-12 13:16:33 +0530

    add tailwind to icons

--- apps/excalidraw-frontend/src/components/IconBar.tsx
+++ apps/excalidraw-frontend/src/components/IconBar.tsx
@@ -1,39 +1,37 @@
-import { Circle, Pencil, RectangleHorizontal } from "lucide-react"
+import { Circle, Pencil, RectangleHorizontal } from "lucide-react";
 
 interface IconBarProps {
-    onClick: (tool: string)=> void,
-    selectedTool: string 
+  onClick: (tool: string) => void;
+  selectedTool: string;
 }
-export const IconBar = ({onClick, selectedTool}: IconBarProps) => {
-  return(
-    <div className="absolute top-2 left-1/2 -translate-x-1/2 w-[500px] flex items-center justify-center gap-3 bg-red-400 dark:bg-gray-800 p-2 rounded-lg shadow-md">
-    <Pencil
-      onClick={() => onClick("pencil")}
-      size={64}
-      className={`p-2 rounded-md transition-all ${
-        selectedTool === "pencil"
-          ? "bg-gray-900 text-white scale-125 shadow-lg"
-          : "text-gray-200 hover:scale-110"
-      }`}
-    />
-    <RectangleHorizontal 
-      onClick={() => onClick('rectangle')}
-      size={64}
-      className={`p-2 rounded-md transition-all ${
-        selectedTool === "rectangle"
-          ? "bg-gray-900 text-white scale-125 shadow-lg"
-          : "text-gray-200 hover:scale-110"
-      }`}     
-      />
-    <Circle
-      onClick={() => onClick('circle')}
-      size={64}
-      className={`p-2 rounded-md transition-all ${
-        selectedTool === "circle"
-          ? "bg-gray-900 text-white scale-125 shadow-lg"
-          : "text-gray-200 hover:scale-110"
-      }`}
-    />
-  </div>
-  )
-}
+export const IconBar = ({ onClick, selectedTool }: IconBarProps) => {
+  return (
+    <div className="absolute top-2 left-1/2 -translate-x-1/2 h-10 w-40 flex items-center justify-center gap-6 bg-gray-700 p-2 rounded-lg shadow-md">
+     <div
+      className={`cursor-pointer  ${selectedTool === "pencil" ? "bg-gray-900 px-1 py-1 rounded" : "hover:scale-110"}`}
+     
+     >
+     <Pencil
+        onClick={() => onClick("pencil")}
+        size={`${selectedTool === "pencil" ? "28" : "22"}`}
+         />
+     </div>
+      <div
+        className={`cursor-pointer  ${selectedTool === "rectangle" ? "bg-gray-900 px-0.5 py-0 rounded" : "hover:scale-110"}`}
+      >
+        <RectangleHorizontal
+          onClick={() => onClick("rectangle")}
+          size={`${selectedTool === "rectangle" ? "28" : "22"}`}
+        />
+      </div>
+      <div
+        className={`cursor-pointer  ${selectedTool === "circle" ? "bg-gray-900 rounded-full" : "hover:scale-110"}`}
+      >
+        <Circle
+          onClick={() => onClick("circle")}
+          size={`${selectedTool === "circle" ? "28" : "22"}`}
+        />
+      </div>
+    </div>
+  );
+};
